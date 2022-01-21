ef6d2f7dffd58c42a80bfcbebdb134d7661db9cc
unknown <lindsay.kelly15@gmail.com>
2022-01-21 15:10:30 -0500

    styled components added, mvp met in all areas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import Character from './components/Character';
 
 import './App.css';
 
+const StyledHeader = styled.h1`
+  letter-spacing: 1px;
+  text-shadow: -1px -1px 1px #000, 1px 1px 1px #ccc;
+  border-bottom: 2px solid #443E3E;
+  display: inline-block;
+
+  &:hover {
+    color: #CA7D01;
+    text-shadow: 2px 2px 3px #FFFFFF;
+  }
+`
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -31,7 +43,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <StyledHeader className="Header">Characters</StyledHeader>
       {
         characters.map(info => {
           return <Character info={info} key={info.name} />
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,16 +1,21 @@
 // Write your Character component here
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const CharacterDesc = styled.div`
     color: white;
     border: 3px solid #443E3E;
-    max-width: 53%;
+    border-radius: 8px;
+    max-width: 30%;
     display: flex;
     margin: 1% auto;
     justify-content: space-between;
     align-items: center;
     padding: 0 1%;
+
+    &:hover {
+        border: 6px solid #CA7D01;
+    }
 `
 
 const Character = (props) => {
@@ -18,14 +23,13 @@ const Character = (props) => {
     return (
         <CharacterDesc>
             <h2>{info.name}</h2>
-            &#9187;
-            <p>Birth date: {info.birth_year}</p>
-            &#9187;
-            <p> &#9892;
+            <p> &#127874;
+                DOB: {info.birth_year} <br></br>
+                &#9892;
                 Gender: {info.gender} <br></br>
                 &#128065;&#65039;&#8205;&#128488;&#65039;
                 Eyes: {info.eye_color} <br></br>
-                &#129459;
+                &#128113;&#127996;&#8205;&#9794;&#65039;
                 Hair: {info.hair_color}
             </p>
         </CharacterDesc>
